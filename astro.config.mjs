a93380c5817d9ebb89e21e7e1c15bc67c0a47486
Ahmet <ayavuz0524@gmail.com>
2025-10-02 22:50:35 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://successodysseyhub.com',
+   trailingSlash: 'always', // veya 'never' veya 'ignore'
 
   integrations: [
     sitemap({
